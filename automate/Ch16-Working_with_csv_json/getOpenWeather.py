32f442171336ebc88396b81e185b3e204d4478b8
funkjerry <steven67889110@gmail.com>
2021-09-12 17:47:52 +0800

    Ch16 finish

--- automate/Ch16-Working_with_csv_json/getOpenWeather.py
+++ automate/Ch16-Working_with_csv_json/getOpenWeather.py
@@ -5,27 +5,33 @@ import json
 import requests
 import sys
 
-APPID = 'Your_AppID_Here'
+APPID = 'ENTER_YOUR_APPID'
 
 # Compute location from command line arguments
-if len(sys.argv) < 2:
+# 只要該城市名字並非重複，可以只輸入城市
+if len(sys.argv) < 1:
     print('Usage: quickweather.py city_name, 2-letter_country_code')
     sys.exit()
-location = ' '.join(sys.argv[1:])
+location = ''.join(sys.argv[1:])
+print(location)
+
 # Download the JSON data from openweathermap.org's API
 # 我將網址修改成當前天氣結果/weather並加入&units=metric將溫度轉成攝氏
 url = 'http://api.openweathermap.org/data/2.5/weather?q=%s&APPID=%s&units=metric' % (
     location, APPID)
 response = requests.get(url)
 response.raise_for_status()
+url_2 = 'http://api.openweathermap.org/data/2.5/forecast?q=%s&APPID=%s&units=metric' % (
+    location, APPID)
+response_2 = requests.get(url_2)
+response_2.raise_for_status()
 
 # Load JSON data into Python variable.
 weatherData = json.loads(response.text)
+forecastData = json.loads(response_2.text)
 
-# Uncomment to see the raw JSON text:
 # print(response.text)
 coord = weatherData['coord']
-coord['lon']
 print(f'longitude of {location} is {coord["lon"]}')
 print(f'latitude of {location} is {coord["lat"]}')
 w = weatherData['weather']
@@ -34,3 +40,8 @@ main = weatherData['main']
 print(f'Current weather in {location}:\n'
       f'{w[0]["main"]}-{w[0]["description"]}, '
       f'temperature is {main["temp"]}\N{DEGREE SIGN}C, and feels like {main["feels_like"]}\N{DEGREE SIGN}C')
+# probability of precipitation
+for i in range(15):
+    data = forecastData['list'][i]
+    print(
+        f"{data['dt_txt']} {data['weather'][0]['description']}/pop: {str(data['pop'])}")
